2d73d73b71cb612595987137f56e695c4e4aa1a1
Bruno Veiga <veiga.munoz@gmail.com>
2024-10-17 12:50:35 +0200

    Añadida pagina de sign in

--- app/src/main/java/com/BrunoV/nicestart/Login.java
+++ app/src/main/java/com/BrunoV/nicestart/Login.java
@@ -1,6 +1,8 @@
 package com.BrunoV.nicestart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +10,7 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class Login extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +22,9 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+    }
+    public void openSignup(View v) {
+        Intent intent = new Intent(Login.this,Signup.class);
+        startActivity(intent);
     }
 }
